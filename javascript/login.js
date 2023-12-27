2911194c5dc543deadbba2e27c38d583953e421f
Mohamed Emad <mohamed.emad45621@gmail.com>
2023-12-27 19:59:45 +0200

    admin page edits

--- javascript/login.js
+++ javascript/login.js
@@ -1,17 +1,6 @@
 
-// function searchCars() {
-//   const salaryFrom = document.getElementById("salary-from").value;
-//   const salaryTo = document.getElementById("salary-to").value;
-//   const carType = document.getElementById("car-type").value;
-//   const location = document.getElementById("location").value;
-
-//   console.log(
-//     `Searching for cars with salary range from ${salaryFrom} to ${salaryTo}, type: ${carType}, location: ${location}`
-//   );
-// }
-
 function login() {
-  window.location.href = "login.html";
+  window.location.href = "../gg.php";
 }
 
 function searchCars() {
@@ -21,26 +10,5 @@ function searchCars() {
 
 
 
-// const signUpButton = document.getElementById('signUp');
-//         const signInButton = document.getElementById('signIn');
-//         const container = document.getElementById('container');
-//         const signUpForm = document.querySelector('.sign-up-container');
-//         const signInForm = document.querySelector('.sign-in-container');
 
-//         signUpButton.addEventListener('click', () => {
-//             container.classList.add("right-panel-active");
-//         });
-
-//         signInButton.addEventListener('click', () => {
-//             container.classList.remove("right-panel-active");
-//         });
-        
-//         function newuser()
-//         {
-//     //    window.location.href = "/login.html";
-//         }
-//     function olduser()
-// {
-//     //this.$router.push('/admin');
-// }  
     
