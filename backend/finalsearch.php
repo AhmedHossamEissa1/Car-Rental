bc9da38679d774d833feb25a1e9fee165acc6b5c
Mohamed Emad <mohamed.emad45621@gmail.com>
2023-12-28 19:38:45 +0200

    Update finalsearch.php

--- backend/finalsearch.php
+++ backend/finalsearch.php
@@ -6,17 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/result.css">
     <title>Search</title>
-
 </head>
 
 <body>
-
     <?php
-
-    function drawTable($row_new, $query_new)  //function to draw tables
+    function drawTable($row_new, $query_new)
     {
-
-        $numColumns = mysqli_num_fields($query_new); //get number of columns
+        $numColumns = mysqli_num_fields($query_new);
         echo '<table style="width: 96px;%">';
         echo '<tr>';
         for ($i = 0; $i < $numColumns; $i++) {
@@ -35,11 +31,8 @@
             } else {
                 echo '<There is no such result match in database';
             }
-        } while ($row_new = mysqli_fetch_assoc($query_new));   //put all rows in table
-
+        } while ($row_new = mysqli_fetch_assoc($query_new));
     }
-
-
 
     session_start();
     $serverName = "localhost";
@@ -57,29 +50,25 @@
         $inp = $_POST['text'];
         $query = mysqli_query(
             $con,
-            "SELECT car.year,car.model,car.car_status,car.price_per_day,car.office_id
-                    FROM car
-                    WHERE '$inp' = `year` OR '$inp' = `car_status` OR '$inp' = `plate_id` OR model LIKE '%$inp%'  
-                    OR '$inp' = `price_per_day`
-                    "
+            "SELECT car.year, car.model, car.car_status, car.price_per_day, car.office_id
+            FROM car
+            WHERE '$inp' = `year` OR '$inp' = `car_status` OR '$inp' = `plate_id` OR model LIKE '%$inp%'
+            OR '$inp' = `price_per_day`
+            "
         );
+
         if ($query) {
             $row = mysqli_fetch_assoc($query);
-            // echo "result of search: ";
-            // echo '<br>';
+            echo '<br>';
+            echo '<br>';
             drawTable($row, $query);
         } else {
             echo '<br>';
             echo "Failed to Search";
         }
-    }
-
-  
-
-    else if(isset($_POST['search2'])){
+    } elseif (isset($_POST['search2'])) {
         $year = $_POST['year'];
         $model = $_POST['model'];
-        // $status = $_POST['status'];
         $price_from = $_POST['price-from'];
         $price_to = $_POST['price-to'];
         $country = $_POST['country'];
@@ -88,25 +77,22 @@
         $query = mysqli_query(
             $con,
             "SELECT c.`year`,c.model,c.car_status,c.price_per_day,c.office_id
-        FROM car as c
-    join OFFICE as o on o.office_id=c.office_id
-    WHERE model LIKE '$model%' and `year`='$year' and price_per_day between '$price_from' and '$price_to' and country='$country' and `state`='$state' "
+            FROM car as c
+            JOIN OFFICE as o on o.office_id=c.office_id
+            WHERE model LIKE '$model%' and `year`='$year' and price_per_day between '$price_from' and '$price_to' and country='$country' and `state`='$state' "
         );
 
-    }
-
-    if ($query) {
-        $row = mysqli_fetch_assoc($query);
-        // echo "result of search: ";
-        echo '<br>';
-        echo '<br>';
-        drawTable($row, $query);
-    } else {
-        echo '<br>';
-        echo "Failed to Search";
+        if ($query) {
+            $row = mysqli_fetch_assoc($query);
+            echo '<br>';
+            echo '<br>';
+            drawTable($row, $query);
+        } else {
+            echo '<br>';
+            echo "Failed to Search";
+        }
     }
     ?>
-
 </body>
 
 </html>
